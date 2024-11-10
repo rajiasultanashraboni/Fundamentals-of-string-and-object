5eab5018b5bb555798faa8544d4603e0c1d81165
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-11 00:23:20 +0600

    array is muteable thats why it can change but string is immuteable thats why it can not change

--- string.js
+++ string.js
@@ -1,3 +1,13 @@
 const district = "brahmanbaria";
 const division = "chittagong";
 const country = `Bangladesh`;
+
+let numbers = [10, 20, 30, 40, 50, 60];
+console.log(numbers);
+numbers[0] = 20; //array is mutable thats why it can change
+console.log(numbers);
+
+let city = "Dhaka";
+console.log(city);
+city[0] = "F"; //string is immuteable thats why it can not change
+console.log(city);
