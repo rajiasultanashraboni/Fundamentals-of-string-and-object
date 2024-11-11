6016436bd6ac33c9b113d08075778189ad746e51
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-11 13:19:53 +0600

    concat er maddhome amra ek ba ekadhik string ke jukto kore ekta string e convert korte pari

---
+++ concat.js
@@ -0,0 +1,6 @@
+let text1 = "hello";
+let text2 = "world";
+// let result = text1.concat(text2);
+// let result = text1 + " " + text2;
+let result = text1.concat(" ").concat(text2);
+console.log(result);
