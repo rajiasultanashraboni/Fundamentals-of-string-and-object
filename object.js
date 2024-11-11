1b95acc943a618b272edc71cb2c8b9b335453c23
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-11 14:55:52 +0600

    ekta object e amra onek dhoroner data property akare rakhte pari.jemon array onno object ba function o rakhte pari

---
+++ object.js
@@ -0,0 +1,20 @@
+const bottle = {
+  brand: "apple",
+  isClean: true,
+  weight: 47,
+  color: "white",
+};
+
+// console.log(bottle);
+
+const subject = {
+  subject: "bangla",
+  teacher: "ferdousi",
+  examDate: "30 feb",
+  Chapter: ["first", "second", "third"],
+  group: {
+    science: "subject",
+    commerce: "english",
+    arts: "bangla",
+  },
+};
