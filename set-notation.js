761c7d5ecee6882ea507356f85b76fd10348b024
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-11 21:01:57 +0600

    set property using dot notation and bracket notation

---
+++ set-notation.js
@@ -0,0 +1,19 @@
+const person = {
+  name: "shraboni",
+  age: 25,
+  profession: "developer",
+  weight: 38,
+  height: 5,
+  salary: 25000,
+  "fav places": ["bandarban", "katabon", "modhubon"],
+};
+
+person.salary = 26000;
+person["age"] = 26;
+person["fav places"] = ["maldive", "balli", "kuakata"];
+
+console.log(person);
+
+const prop = "profession";
+person[prop] = "devops";
+console.log(person);
