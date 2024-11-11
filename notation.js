fe1515b7de57cce8f7ae743dac349b300c39bfc9
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-11 20:44:13 +0600

    here we can practice that how to get property value from a object using dot notation and bracket notation

---
+++ notation.js
@@ -0,0 +1,18 @@
+const person = {
+  name: "shraboni",
+  age: 25,
+  profession: "developer",
+  weight: 38,
+  height: 5,
+};
+
+// console.log(person);
+
+//get property value by dot notation
+const prof = person.profession;
+// console.log(prof);
+
+//get property value by bracket notation
+const prop = person["profession"];
+// console.log(person["profession"]);
+console.log(prop);
